1f1434ed80503a74e6b7cdddc90da4d55c956152
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-06 19:29:20 +0200

    telegram update

--- __tests__/index.unit.spec.ts
+++ __tests__/index.unit.spec.ts
@@ -1,10 +1,8 @@
 import { Contract } from "../assembly";
 import { VMContext } from "near-mock-vm";
-import { Profile, Verification } from "../assembly/models";
 import { u128} from "near-sdk-as";
 
 let contractt: Contract;
-let verr: Verification;
 const CREATOR_ACCOUNT_ID = "someone.NEAR";
 const CURRENT_ACCOUNT_ID = "someone.NEAR";
 const PREDECESSOR_ACCOUNT_ID = "Owner.testnet";
@@ -17,135 +15,154 @@ beforeAll(() => {
 });
 
 
-describe("Creating a profile", () => {
-    test("'Account Created Successfully'", () => {
+// describe("Creating a profile", () => {
+//     test("'Account Created Successfully'", () => {
 
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id(CREATOR_ACCOUNT_ID)
-        let newProfile = new Profile("Name", "bio", "email", "website", "imageURL", 455454)
-        let createdProfile = contractt.createProfile(newProfile)
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id(CREATOR_ACCOUNT_ID)
+//         let newProfile = new Profile("Name", "bio", "email", "website", "imageURL", 455454)
+//         let createdProfile = contractt.createProfile(newProfile)
         
-        expect(createdProfile.accountID).toBe(CREATOR_ACCOUNT_ID)
+//         expect(createdProfile.accountID).toBe(CREATOR_ACCOUNT_ID)
 
-    });
-});
+//     });
+// });
 
-describe("Getting a profile", () => {
-    test("returns the account that is linked to a given NEAR ID", () => {
+// describe("Getting a profile", () => {
+//     test("returns the account that is linked to a given NEAR ID", () => {
 
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id("someone.NEAR")
-        let newProfile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let newProfile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
 
-        expect(contractt.createProfile(newProfile).accountID).toBe(newProfile.accountID);
+//         expect(contractt.createProfile(newProfile).accountID).toBe(newProfile.accountID);
 
-        let createdProfile = contractt.getProfile(CURRENT_ACCOUNT_ID);
+//         let createdProfile = contractt.getProfile(CURRENT_ACCOUNT_ID);
 
-        expect(createdProfile.name).toBe(newProfile.name);
-        expect(createdProfile.bio).toBe(newProfile.bio);
-        expect(createdProfile.email).toBe(newProfile.email);
-        expect(createdProfile.website).toBe(newProfile.website);
-        expect(createdProfile.imageURL).toBe(newProfile.imageURL);
-        expect(createdProfile.dateOfBirth).toBe(newProfile.dateOfBirth);
-    });
-});
+//         expect(createdProfile.name).toBe(newProfile.name);
+//         expect(createdProfile.bio).toBe(newProfile.bio);
+//         expect(createdProfile.email).toBe(newProfile.email);
+//         expect(createdProfile.website).toBe(newProfile.website);
+//         expect(createdProfile.imageURL).toBe(newProfile.imageURL);
+//         expect(createdProfile.dateOfBirth).toBe(newProfile.dateOfBirth);
+//     });
+// });
 
-describe("Verify an account", () => {
+// describe("Verify an account", () => {
     
-    test("Account is verified", () => {
+//     test("Account is verified", () => {
 
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id(CURRENT_ACCOUNT_ID)
-        VMContext.setPredecessor_account_id(PREDECESSOR_ACCOUNT_ID)
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id(CURRENT_ACCOUNT_ID)
+//         VMContext.setPredecessor_account_id(PREDECESSOR_ACCOUNT_ID)
 
-        let verification = new Verification("linkedIn", 3 , "WTF! It's Wonderful" )
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 89895585)
-        contractt.createProfile(profile)
-        let createdProfile = contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
+//         let verification = new Verification("linkedIn", 3 , "WTF! It's Wonderful" )
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 89895585)
+//         contractt.createProfile(profile)
+//         let createdProfile = contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
     
-        expect(createdProfile.accountID).toBe(CURRENT_ACCOUNT_ID)
+//         expect(createdProfile.accountID).toBe(CURRENT_ACCOUNT_ID)
 
-    });
-});
+//     });
+// });
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-describe("Deposit test", () => {
-    test("Check if there is enough NEAR deposit", () => {
-        VMContext.setPredecessor_account_id("Owner.testnet")
-        VMContext.setAttached_deposit(u128.from(0.5))
-        expect(() =>{contractt.verifyAccount(CURRENT_ACCOUNT_ID, verr)}).toThrow('1 NEAR is required')
-    });
-});
+// describe("Deposit test", () => {
+//     test("Check if there is enough NEAR deposit", () => {
+//         VMContext.setPredecessor_account_id("Owner.testnet")
+//         VMContext.setAttached_deposit(u128.from(0.5))
+//         expect(() =>{contractt.verifyAccount(CURRENT_ACCOUNT_ID, verr)}).toThrow('1 NEAR is required')
+//     });
+// });
 
-describe("Check account creation", () => {
-    test("Check if The account is created successfully", () => {
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id("someone.NEAR")
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
-        let testprofile = contractt.createProfile(profile)
-        expect(testprofile.accountID).toBe(CURRENT_ACCOUNT_ID)
-    }); 
-});
+// describe("Check account creation", () => {
+//     test("Check if The account is created successfully", () => {
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         let testprofile = contractt.createProfile(profile)
+//         expect(testprofile.accountID).toBe(CURRENT_ACCOUNT_ID)
+//     }); 
+// });
 
-describe("Check profileList", () => {
-    test("Check if The account is added to profilelist", () => {
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id("someone.NEAR")
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
-        contractt.createProfile(profile)
-        expect(contractt.profilesList.contains(CURRENT_ACCOUNT_ID)).toBeTruthy
-    });
+// describe("Check profileList", () => {
+//     test("Check if The account is added to profilelist", () => {
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         contractt.createProfile(profile)
+//         expect(contractt.profilesList.contains(CURRENT_ACCOUNT_ID)).toBeTruthy
+//     });
     
-});
+// });
 
-describe("Check account id", () => {
-    test("Check if The account id is set correctly", () => {
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id("someone.NEAR")
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
-        contractt.createProfile(profile)
-        let testProfile = contractt.getProfile(CURRENT_ACCOUNT_ID)
-        expect(testProfile.accountID).toBe("someone.NEAR")
-    });
-});
-describe("account verification", () => {
-    test("Check if account verification is set successfully", () => {
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id("someone.NEAR")
-        let verification = new Verification("facebook", 1 , "amazing" )
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
-        contractt.createProfile(profile)
-        VMContext.setPredecessor_account_id("Owner.testnet")
-        let testProfile = contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
-        expect(testProfile.accountID).toBe("someone.NEAR")          
-    });
+// describe("Check account id", () => {
+//     test("Check if The account id is set correctly", () => {
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         contractt.createProfile(profile)
+//         let testProfile = contractt.getProfile(CURRENT_ACCOUNT_ID)
+//         expect(testProfile.accountID).toBe("someone.NEAR")
+//     });
+// });
+// describe("account verification", () => {
+//     test("Check if account verification is set successfully", () => {
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let verification = new Verification("facebook", 1 , "amazing" )
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         contractt.createProfile(profile)
+//         VMContext.setPredecessor_account_id("Owner.testnet")
+//         let testProfile = contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
+//         expect(testProfile.accountID).toBe("someone.NEAR")          
+//     });
     
-});
+// });
 
-describe("Check on verificationList", () => {
-    test("Check if verifications are added to the verificationList successfully", () => {
-        VMContext.setAttached_deposit(u128.from(1))
-        VMContext.setSigner_account_id("someone.NEAR")
-        let verification = new Verification("facebook", 1 , "amazing" )
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
-        contractt.createProfile(profile)
-        VMContext.setPredecessor_account_id("Owner.testnet")
-        let testProfile = contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
-        expect(testProfile.verificationList.length).toBe(1)
-    });
+// describe("Check on verificationList", () => {
+//     test("Check if verifications are added to the verificationList successfully", () => {
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let verification = new Verification("facebook", 1 , "amazing" )
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         contractt.createProfile(profile)
+//         VMContext.setPredecessor_account_id("Owner.testnet")
+//         let testProfile = contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
+//         expect(testProfile.verificationList.length).toBe(1)
+//     });
     
-});
+// });
+
+// describe("isAccountVerified function", () => {
+//     test("Check if the function isAccountVerified works probably", () => {
+//         VMContext.setAttached_deposit(u128.from(1))
+//         VMContext.setSigner_account_id("someone.NEAR")
+//         let verification = new Verification("facebook", 1 , "amazing" )
+//         let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
+//         contractt.createProfile(profile)
+//         VMContext.setPredecessor_account_id("Owner.testnet")
+//         contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
+//         expect(contractt.isAccountVerified(CURRENT_ACCOUNT_ID)).toBeTruthy()
+//     }); 
+// });
 
 describe("isAccountVerified function", () => {
     test("Check if the function isAccountVerified works probably", () => {
-        VMContext.setAttached_deposit(u128.from(1))
         VMContext.setSigner_account_id("someone.NEAR")
-        let verification = new Verification("facebook", 1 , "amazing" )
-        let profile = new Profile("Name", "bio", "email", "website", "imageURL", 11111)
-        contractt.createProfile(profile)
+        contractt.createProfile()
+        expect(contractt.profilesList.contains(CURRENT_ACCOUNT_ID)).toBeTruthy()
+    }); 
+});
+
+describe("isAccountVerified function", () => {
+    test("2", () => {
+        VMContext.setSigner_account_id("someone.NEAR")
+        contractt.createProfile()
+        VMContext.setAttached_deposit(u128.from(1))
         VMContext.setPredecessor_account_id("Owner.testnet")
-        contractt.verifyAccount(CURRENT_ACCOUNT_ID,verification)
-        expect(contractt.isAccountVerified(CURRENT_ACCOUNT_ID)).toBeTruthy()
+        contractt.verifyAccount("someone.NEAR" , 5)
+        expect(contractt.isAccountVerified("someone.NEAR")).toBe(5)
     }); 
 });
--- assembly/index.ts
+++ assembly/index.ts
@@ -1,32 +1,37 @@
 import { context, PersistentMap, u128} from "near-sdk-as";
-
+export const enum verificationType {
+  New = 0,
+  Pending = 1,
+  Rejected = 2,
+  Verified = 3,
+  Spam = 4
+}
 @nearBindgen
 export class Contract {
   // A List that contains all the registered profiles
-  profilesList : PersistentMap<string, bool> = new PersistentMap<string, bool>("P");
+  profilesList : PersistentMap<string, verificationType> = new PersistentMap<string, verificationType>("P");
 
   // This functions checks if the profile is already linked to this near account or not, if it isn't then it creates as new profile
   @mutateState()
   createProfile() : string {
     let accountID = context.sender
     assert(!this.profilesList.contains(accountID), "This NEAR ID is already linked to another account")
-    this.profilesList.set(accountID, false)
+    this.profilesList.set(accountID, verificationType.New)
     return accountID
   }
 
-  // This function will be called by the front-end to add a verification method and only the "owner/admin" can access it
   // assuming that the admin id is Owner.testnet
   @mutateState()
-  verifyAccount(accountID:string) : string{
+  verifyAccount(accountID : string, verification : verificationType ) : string{
     let adminProfile = "Owner.testnet"
     assert(this.profilesList.contains(accountID), "This NEAR ID is missing")
     assert(context.predecessor == adminProfile, "Access Denied")
-    this.profilesList.set(accountID, true)
+    this.profilesList.set(accountID, verification)
     return accountID
   }
 
   // This function acts as API to know if the account is Verified or not
-  isAccountVerified(accountID : string): bool{
+  isAccountVerified(accountID : string): verificationType{
     assert(context.attachedDeposit == u128.from(1), "1 NEAR is required")
     return this.profilesList.getSome(accountID)
   }
